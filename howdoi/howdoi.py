dffed7fa161dcbc5e02d026478b30792c53e03f7
Sean Saville <sean.saville@hotmail.co.uk>
2016-11-25 12:17:09 +0000

    Restructured project to pull InvertedIndex out of main module

---
+++ howdoi/howdoi.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+
+"""
+howdoi
+
+Search a database of small pieces of information and output the results in the
+terminal.
+"""
+
+
+import sys
+
+from invertedindex import InvertedIndex
+
+
+def main(argv):
+    index = InvertedIndex()
+    index.load_file("howdoi.json")
+
+    query_tags = argv[1:]
+
+    results = index.search(query_tags)
+    if results:
+        for result in results:
+            # The term dictionary is keyed by integers, but the JSON serialiser
+            # converts these to strings, so treat each document ID as a string.
+            print(index.term_dict[str(result)])
+    else:
+        print("Couldn't find anything matching those tags!")
+
+
+if __name__ == "__main__":
+    main(sys.argv)
--- howdoi/invertedindex.py
+++ howdoi/invertedindex.py
@@ -1,15 +1,4 @@
-#!/usr/bin/python3
-
-"""
-howdoi
-
-Search a database of small pieces of information and output the results in the
-terminal.
-"""
-
 import json
-import sys
-
 
 class InvertedIndex(object):
     """
@@ -95,23 +84,3 @@ class InvertedIndex(object):
             matches = matches.intersection(postings_list)
 
         return matches
-
-
-def main(argv):
-    index = InvertedIndex()
-    index.load_file("howdoi.json")
-
-    query_tags = argv[1:]
-
-    results = index.search(query_tags)
-    if results:
-        for result in results:
-            # The term dictionary is keyed by integers, but the JSON serialiser
-            # converts these to strings, so treat each document ID as a string.
-            print(index.term_dict[str(result)])
-    else:
-        print("Couldn't find anything matching those tags!")
-
-
-if __name__ == "__main__":
-    main(sys.argv)
